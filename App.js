dcc2ffc411150651b415a80921b74214a525a9c2
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-14 19:25:11 -0400

    minor stylesheet express.static issue fixed

--- App.js
+++ App.js
@@ -7,17 +7,19 @@ const { db } = require("./models");
 const app = express();
 app.use(morgan('dev'));
 // FOR GRABBING CONTENTS FROM public DIR
-app.use(express.static(__dirname + "/public/stylesheets"));
-
-db.authenticate().
-then(() => {
-    console.log('FINALLY connected to database wikistack');
-})
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     const content='';
     res.send(layout(content));
 });
 
+db.authenticate().
+then(() => {
+    console.log('FINALLY connected to database wikistack');
+})
+
 app.listen(3000);
+
+
 
