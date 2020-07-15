78c58d4c3d1bf84f76c5891ac675f0f8254735be
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-15 11:53:47 -0400

    ADDED comments

--- routes/wiki.js
+++ routes/wiki.js
@@ -4,14 +4,17 @@ const { addPage } = require('../views');
 
 module.exports = router;
 
+// RETRIEVES ALL WIKI PAGES
 router.get('/', (req, res, next) => {
     res.send('got to GET /wiki/');
 });
 
+// SUBMIT A NEW PAGE TO THE DB
 router.post('/', (req, res, next) => {
     res.send('got to POST /wiki/');
 });
 
+// RETRIEVE THE addPage FORM
 router.get('/add', (req, res, next) => {
     res.send(addPage());
 });
