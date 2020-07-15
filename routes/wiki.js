e2a2d892421e8f357f03cd3fa8e0da8798f1e748
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-15 18:33:03 -0400

    RESTful ROUTING - wiki.js router.post() request to RESTful route

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { Page } = require("../models");  
 const { addPage } = require('../views');
-
-module.exports = router;
 
 // RETRIEVES ALL WIKI PAGES
 router.get('/', (req, res, next) => {
@@ -10,8 +9,17 @@ router.get('/', (req, res, next) => {
 });
 
 // SUBMIT A NEW PAGE TO THE DB
-router.post('/', (req, res, next) => {
-    res.send('got to POST /wiki/');
+router.post('/', async (req, res, next) => {
+    // res.send('got to POST /wiki/');
+    // res.json(req.body); // => suppose to see the JSON in the browser
+    const page = new Page ({
+        title: req.body.title,
+        content: req.body.content,
+    });
+    try {
+        await page.save();
+        res.redirect('/');
+    } catch (err) {next(err)}
 });
 
 // RETRIEVE THE addPage FORM
