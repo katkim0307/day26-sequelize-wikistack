e572c006a8afe3925cbb0ba15fe8ff05082ccba9
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-15 11:43:26 -0400

    ADDED routes for /wiki/ and /wiki/add/

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ const morgan = require("morgan");
 const { views } = require("./views");
 const layout = require("./views/layout");
 const { db } = require("./models");
+const models = require("./models");
+const wikiRouter = require("./routes/wiki");
+const userRouter = require("./routes/user");
 
 const app = express();
 app.use(morgan('dev'));
@@ -14,12 +17,30 @@ then(() => {
     console.log('FINALLY connected to database wikistack');
 })
 
+app.use('/wiki', wikiRouter);
+
 app.get("/", (req, res) => {
     const content='';
     res.send(layout(content));
 });
 
-app.listen(3000);
+const PORT = 3000;
+
+const init = async () => {
+    // SYNC BOTH USER AND PAGE MODELS
+    // .sync() is an asynchronous operation and returns a promise
+    await models.User.sync()
+    await models.Page.sync()
+    // THIS DROPS ALL TABLES THEN RECREATES THEM BASED ON THE JS DEFINITION
+    models.db.sync({force: true})
+    app.listen(PORT, () => {
+        console.log(`SERVER IS LISTENING ON PORT ${PORT}!`);
+    });
+}
+
+init();
+
+// app.listen(3000);
 
 
 
