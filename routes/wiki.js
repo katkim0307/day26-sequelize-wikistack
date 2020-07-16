791725632e14bba3d6570b1394484056e1765284
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-16 12:13:08 -0400

    CREATED a page route for a specific slug by quering models

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Page } = require("../models");  
 const { addPage } = require('../views');
+// const { regexp } = require('sequelize/types/lib/operators');
 
 // RETRIEVES ALL WIKI PAGES
 router.get('/', (req, res, next) => {
@@ -11,7 +12,7 @@ router.get('/', (req, res, next) => {
 // SUBMIT A NEW PAGE TO THE DB
 router.post('/', async (req, res, next) => {
     // res.send('got to POST /wiki/');
-    // res.json(req.body); // => suppose to see the JSON in the browser
+    // res.json(req.body); // => test to see the JSON in the browser
     const page = new Page ({
         title: req.body.title,
         content: req.body.content,
@@ -25,6 +26,20 @@ router.post('/', async (req, res, next) => {
 // RETRIEVE THE addPage FORM
 router.get('/add', (req, res, next) => {
     res.send(addPage());
+});
+
+// RETRIEVE A SPECIFIC wiki/slub PAGE BY QUERING MODELS 
+router.get('/:slug', async (req, res, next) => {
+    // res.send(`hit dynamic route at ${req.params.slug}`);
+    try {
+        const page = await Page.findOne ({
+            where: {
+                slug: req.params.slug,
+            }
+        });
+        res.json(page);
+    } 
+    catch (err) { next(err) }
 });
 
 module.exports = router;
