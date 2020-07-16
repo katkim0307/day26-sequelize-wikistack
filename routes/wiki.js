f0a11588a45932ac04ca383b6023c29b3cc8a1eb
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-16 14:52:58 -0400

    sending to the wikipage after a new post is created

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { Page } = require("../models");  
+const { Page } = require("../models");
 const { addPage } = require('../views');
+const { wikiPage } = require('../views');
 // const { regexp } = require('sequelize/types/lib/operators');
 
 // RETRIEVES ALL WIKI PAGES
@@ -13,15 +14,16 @@ router.get('/', (req, res, next) => {
 router.post('/', async (req, res, next) => {
     // res.send('got to POST /wiki/');
     // res.json(req.body); // => test to see the JSON in the browser
-    const page = new Page ({
+    const page = new Page({
         title: req.body.title,
         content: req.body.content,
         status: req.body.status,
     });
     try {
         await page.save();
-        res.redirect('/');
-    } catch (err) {next(err)}
+        // res.redirect('/');
+        res.redirect(`/wiki/${page.slug}`);
+    } catch (err) { next(err) }
 });
 
 // RETRIEVE THE addPage FORM
@@ -29,17 +31,18 @@ router.get('/add', (req, res, next) => {
     res.send(addPage());
 });
 
-// RETRIEVE A SPECIFIC wiki/slub PAGE BY QUERING MODELS 
+// RETRIEVE A SPECIFIC wiki/slug PAGE BY QUERING MODELS 
 router.get('/:slug', async (req, res, next) => {
     // res.send(`hit dynamic route at ${req.params.slug}`);
     try {
-        const page = await Page.findOne ({
+        const page = await Page.findOne({
             where: {
                 slug: req.params.slug,
             }
         });
-        res.json(page);
-    } 
+        //res.json(page);
+        res.send(wikiPage(page));
+    }
     catch (err) { next(err) }
 });
 
