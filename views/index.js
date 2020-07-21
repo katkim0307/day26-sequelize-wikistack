9f36a28540caded0e0f85f81f52662237a53b1e0
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-21 19:05:13 -0400

    ADDED notFoundPage and errorPage

--- views/index.js
+++ views/index.js
@@ -4,5 +4,7 @@ const main = require("./main");
 const userList = require("./userList");
 const userPages = require("./userPages");
 const wikiPage = require("./wikiPage");
+const notFoundPage = require("./notFoundPage");
+const errorPage = require("./errorPage");
 
-module.exports = { addPage, editPage, main, userList, userPages, wikiPage };
+module.exports = { addPage, editPage, main, userList, userPages, wikiPage, notFoundPage, errorPage };
